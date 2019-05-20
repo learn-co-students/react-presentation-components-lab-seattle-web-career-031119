a0b52f3197411d8e773f45b24b62d9210f434d84
Rylan Bauermeister <apple@MacBook-Pro-6.local>
2019-05-20 09:05:52 -0700

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,25 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react'
+
+export default class SimpleComponent extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      mood: this.props.mood || 'happy'
+    }
+
+    this.toggleMood = this.toggleMood.bind(this)
+  }
+
+  toggleMood() {
+    this.setState({
+      mood: this.state.mood === 'happy' ? 'sad' : 'happy'
+    })
+  }
+
+  render(){
+    return <div onClick={this.toggleMood}>{this.state.mood}</div>;
+  }
+}
